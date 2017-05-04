b870f2c605d048d4ee010d6d2ae269d471bef3c0
Benjamin Casey <benjamincasey55@gmail.com>
2017-05-05 00:14:53 +1000

    Quiz Question

---
+++ 1.4a_Quiz_Question_5_Ben/1.4a_Quiz_Question_5_Ben/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int doubleNumber(int x)
+{
+	return 2 * x;
+}
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+	int x;
+	std::cin >> x;
+	std::cout << std::endl;
+	std::cout << "This is double your number: ";
+	std::cout << doubleNumber(x);
+	int y;
+	std::cin >> y;
+	return 0;
+}
